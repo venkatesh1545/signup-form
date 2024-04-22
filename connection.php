ee530f00a3d9230a095ed3409798fdd1b51f75f4
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2024-04-23 02:21:42 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","","login_database");
-    if(mysqli_connect_error()){
-        echo"<script>alert('cannot connect to the database');</script>";
-    }
-?>
